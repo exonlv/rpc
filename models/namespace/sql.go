911ff62c181a7179f22357c398e03f06a6880ad7
Sergey Fedotenkov <wired.required@gmail.com>
2017-01-18 01:58:02 +0300

    Refactor all update and insert methods.

--- models/namespace/sql.go
+++ models/namespace/sql.go
@@ -1,17 +1,22 @@
 package namespace
 
+import (
+	"database/sql"
+	"errors"
+)
+
 // Add (Namespace, *bool) - Добавление новой записи в таблице Namespace. Передаются только: label, user_id
 func (_ *Namespace) Add(ns Namespace, ok *bool) error {
-	_, err := db.Exec("INSERT INTO namespaces(label, user_id) VALUES($1, $2)", ns.Label, ns.UserID)
+	query := "INSERT INTO namespaces(label, user_id) VALUES($1, $2)"
+	err := queryExecutionHandler(query, ns.Label, ns.UserID)
 	*ok = checkErr(err)
 	return err
 }
 
 // Delete (Namespace, *bool) - изменение removed -> true
 func (_ *Namespace) Delete(ns Namespace, ok *bool) error {
-	row, err := db.Exec("UPDATE namespaces SET removed=TRUE where id=$1", ns.ID)
-	*ok = checkErr(err)
-	err = rowNumbersHandler(row)
+	query := "UPDATE namespaces SET removed=TRUE where id=$1"
+	err := queryExecutionHandler(query, ns.ID)
 	*ok = checkErr(err)
 	return err
 }
@@ -67,45 +72,62 @@ func (_ *Namespace) Get(id string, resp *Namespace) error {
 
 // Activate(id string, *bool) - изменение active -> true
 func (_ *Namespace) Activate(id string, ok *bool) error {
-	row, err := db.Exec("UPDATE namespaces SET active=TRUE where id=$1", id)
-	*ok = checkErr(err)
-	err = rowNumbersHandler(row)
+	query := "UPDATE namespaces SET active=TRUE where id=$1"
+	err := queryExecutionHandler(query, id)
 	*ok = checkErr(err)
 	return err
 }
 
 // Deactivate (id string, *bool) - изменение active -> false
 func (_ *Namespace) Deactivate(id string, ok *bool) error {
-	row, err := db.Exec("UPDATE namespaces SET active=FALSE where id=$1", id)
-	*ok = checkErr(err)
-	err = rowNumbersHandler(row)
+	query := "UPDATE namespaces SET active=FALSE where id=$1"
+	err := queryExecutionHandler(query, id)
 	*ok = checkErr(err)
 	return err
 }
 
 // CreatedInKube (id string, *bool) - изменение kube_exist -> true
 func (_ *Namespace) CreateInKube(id string, ok *bool) error {
-	row, err := db.Exec("UPDATE namespaces SET kube_exist=TRUE where id=$1", id)
-	*ok = checkErr(err)
-	err = rowNumbersHandler(row)
+	query := "UPDATE namespaces SET kube_exist=TRUE where id=$1"
+	err := queryExecutionHandler(query, id)
 	*ok = checkErr(err)
 	return err
 }
 
 // DeletedInKube (id string, *bool) - изменение kube_exist -> false
 func (_ *Namespace) DeletedInKube(id string, ok *bool) error {
-	row, err := db.Exec("UPDATE namespaces SET kube_exist=FALSE where id=$1", id)
-	*ok = checkErr(err)
-	err = rowNumbersHandler(row)
+	query := "UPDATE namespaces SET kube_exist=FALSE where id=$1"
+	err := queryExecutionHandler(query, id)
 	*ok = checkErr(err)
 	return err
 }
 
 // Rename (Namespace, *bool) - изменение label
 func (_ *Namespace) Rename(ns Namespace, ok *bool) error {
-	row, err := db.Exec("UPDATE namespaces SET label=$1 where id=$2", ns.Label, ns.ID)
-	*ok = checkErr(err)
-	err = rowNumbersHandler(row)
+	query := "UPDATE namespaces SET label=$1 where id=$2"
+	err := queryExecutionHandler(query, ns.Label, ns.ID)
 	*ok = checkErr(err)
 	return err
+}
+
+// queryExecutionHandler (query string, args ...interface{}) error
+// query - sql запрос типа "UPDATE namespaces SET label=$1 where id=$2" где $1 и $2 подставляются из args
+// args - аргументы встраиваемые в sql запрос(Порядок аргументов важен!)
+func queryExecutionHandler(query string, args ...interface{}) error {
+	row, err := db.Exec(query, args...)
+	err = rowNumbersHandler(row)
+	return err
+}
+
+// Проверяет колличество обработаных записей, если не было обработано ни одной - возвращает ошибку noRowsProcessedError, иначе nil.
+func rowNumbersHandler(row sql.Result) error {
+	noRowsProcessedError := errors.New("Failed to update/create the namespace. Maybe there is no namespace with such ID in the database.")
+	rowsAffected, err := row.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected < 1 {
+		return noRowsProcessedError
+	}
+	return nil
 }
