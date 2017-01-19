4a58d022d27fd19866546f36f09bea4455b9cbbe
Sergey Fedotenkov <wired.required@gmail.com>
2017-01-19 14:39:56 +0300

    add Volumeserver methods

--- models/volumeserver/sql.go
+++ models/volumeserver/sql.go
@@ -1,1 +1,181 @@
 package volumeserver
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// Add(VolumeServer, *bool) - добавление
+func (_ *VolumeServer) Add(vs VolumeServer, ok *bool) error {
+	var errs []error
+	*ok, errs = vs.checkFields("ip", "path", "memory")
+	if !*ok {
+		return errs[0]
+	}
+	query := "INSERT INTO volumeservers(ip, path, memory) VALUES($1, $2, $3)"
+	err := queryExecutionHandler(query, vs.IP, vs.Path, vs.Memory)
+	*ok = checkErr(err)
+	return err
+}
+
+// Activate(volumeserver_id, *bool) - active -> true
+func (_ *VolumeServer) Activate(volumeserver_id string, ok *bool) error {
+	query := "UPDATE volumeservers SET active=TRUE where volumeserver_id=$1"
+	err := queryExecutionHandler(query, volumeserver_id)
+	*ok = checkErr(err)
+	return err
+}
+
+// Deactivate(volumeserver_id, *bool) - active -> false
+func (_ *VolumeServer) Deactivate(volumeserver_id string, ok *bool) error {
+	query := "UPDATE volumeservers SET active=FALSE where volumeserver_id=$1"
+	err := queryExecutionHandler(query, volumeserver_id)
+	*ok = checkErr(err)
+	return err
+}
+
+// ChangeGroup(VolumeServer, *bool) - изменение group
+func (_ *VolumeServer) ChangeGroup(vs VolumeServer, ok *bool) error {
+	var errs []error
+	*ok, errs = vs.checkFields("groups", "volumeserver_id")
+	if !*ok {
+		return errs[0]
+	}
+	query := "UPDATE volumeservers SET groups=$1 where volumeserver_id=$2"
+	err := queryExecutionHandler(query, vs.Groups, vs.VolumeserverID)
+	*ok = checkErr(err)
+	return err
+}
+
+// ChangeDiskType(VolumeServer, *bool) - изменение disk_type
+func (_ *VolumeServer) ChangeDiskType(vs VolumeServer, ok *bool) error {
+	var errs []error
+	*ok, errs = vs.checkFields("disk_type", "volumeserver_id")
+	if !*ok {
+		return errs[0]
+	}
+	query := "UPDATE volumeservers SET disk_type=$1 where volumeserver_id=$2"
+	err := queryExecutionHandler(query, vs.DiskType, vs.VolumeserverID)
+	*ok = checkErr(err)
+	return err
+}
+
+// ChangePath(VolumeServer, *bool) - изменение path
+func (_ *VolumeServer) ChangePath(vs VolumeServer, ok *bool) error {
+	var errs []error
+	*ok, errs = vs.checkFields("path", "volumeserver_id")
+	if !*ok {
+		return errs[0]
+	}
+	query := "UPDATE volumeservers SET path=$1 where volumeserver_id=$2"
+	err := queryExecutionHandler(query, vs.Path, vs.VolumeserverID)
+	*ok = checkErr(err)
+	return err
+}
+
+// GetByGroup(group, *[]VolumeServer) - все VolumeServer определенной группы
+func (_ *VolumeServer) GetByGroup(group string, resp *[]VolumeServer) error {
+	rows, err := db.Query("SELECT * FROM volumeservers WHERE groups = $1", group)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	volumeservers := make([]VolumeServer, 0)
+	for rows.Next() {
+		vs := VolumeServer{}
+		err := rows.Scan(&vs.VolumeserverID,
+			&vs.IP,
+			&vs.Path,
+			&vs.Memory,
+			&vs.Created,
+			&vs.Active,
+			&vs.Groups,
+			&vs.DiskType,
+		)
+		if err != nil {
+			return err
+		}
+		// fmt.Println(*vs.VolumeserverID)
+		volumeservers = append(volumeservers, vs)
+	}
+	*resp = volumeservers
+	return nil
+}
+
+// GetByActivity(bool active, *[]VolumeServer) - все VolumeServer с определнным active
+func (_ *VolumeServer) GetByActivity(active bool, resp *[]VolumeServer) error {
+	rows, err := db.Query("SELECT * FROM volumeservers WHERE active = $1", active)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	volumeservers := make([]VolumeServer, 0)
+	for rows.Next() {
+		vs := VolumeServer{}
+		err := rows.Scan(&vs.VolumeserverID,
+			&vs.IP,
+			&vs.Path,
+			&vs.Memory,
+			&vs.Created,
+			&vs.Active,
+			&vs.Groups,
+			&vs.DiskType,
+		)
+		if err != nil {
+			return err
+		}
+		volumeservers = append(volumeservers, vs)
+	}
+	*resp = volumeservers
+	return nil
+}
+
+// GetByDiskType(string disk_type, *[]VolumeServer) - все VolumeServer с определенным диском
+func (_ *VolumeServer) GetByDiskType(diskType string, resp *[]VolumeServer) error {
+	rows, err := db.Query("SELECT * FROM volumeservers WHERE disk_type = $1", diskType)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	volumeservers := make([]VolumeServer, 0)
+	for rows.Next() {
+		vs := VolumeServer{}
+		err := rows.Scan(&vs.VolumeserverID,
+			&vs.IP,
+			&vs.Path,
+			&vs.Memory,
+			&vs.Created,
+			&vs.Active,
+			&vs.Groups,
+			&vs.DiskType,
+		)
+		if err != nil {
+			return err
+		}
+		volumeservers = append(volumeservers, vs)
+	}
+	*resp = volumeservers
+	return nil
+}
+
+// queryExecutionHandler (query string, args ...interface{}) error
+// query - sql запрос типа "UPDATE volumeservers SET label=$1 where id=$2" где $1 и $2 подставляются из args
+// args - аргументы встраиваемые в sql запрос(Порядок аргументов важен!)
+func queryExecutionHandler(query string, args ...interface{}) error {
+	_, err := db.Exec(query, args...)
+	// err = rowNumbersHandler(row)
+	return err
+}
+
+// Проверяет колличество обработаных записей, если не было обработано ни одной - возвращает ошибку noRowsProcessedError, иначе nil.
+func rowNumbersHandler(row sql.Result) error {
+	noRowsProcessedError := errors.New("Failed to update/create the volumeserver. Maybe there is no volumeserver in the database.")
+	rowsAffected, err := row.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected < 1 {
+		return noRowsProcessedError
+	}
+	return nil
+}
--- models/volumeserver/volumesever.go
+++ models/volumeserver/volumesever.go
@@ -1,13 +1,58 @@
 package volumeserver
 
-type Volumeservers struct {
+import (
+	"database/sql"
+	"fmt"
+)
+
+type VolumeServer struct {
 	VolumeserverID *string
 	IP             *string
 	Path           *string
-	Name           *string
 	Memory         *int
 	Created        *string
 	Active         *bool
 	Groups         *string
 	DiskType       *string
+}
+
+var db *sql.DB
+
+func (vs *VolumeServer) checkFields(fields ...string) (bool, []error) {
+	errs := make([]error, 0)
+	for _, field := range fields {
+		switch field {
+		case "volumeserver_id":
+			if vs.VolumeserverID == nil {
+				errs = append(errs, fmt.Errorf("Set volumeserver_id field"))
+			}
+		case "ip":
+			if vs.IP == nil {
+				errs = append(errs, fmt.Errorf("Set ip field"))
+			}
+		case "path":
+			if vs.Path == nil {
+				errs = append(errs, fmt.Errorf("Set path field"))
+			}
+		case "memory":
+			if vs.Memory == nil {
+				errs = append(errs, fmt.Errorf("Set memory field"))
+			}
+		case "disk_type":
+			if vs.DiskType == nil {
+				errs = append(errs, fmt.Errorf("Set disk_type field"))
+			}
+		}
+	}
+	if len(errs) != 0 {
+		return false, errs
+	}
+	return true, nil
+}
+
+func checkErr(err error) bool {
+	if err != nil {
+		return false
+	}
+	return true
 }
